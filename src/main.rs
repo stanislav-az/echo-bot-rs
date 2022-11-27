9a1cc99182783832524089a5f6df7284e7e5ef0f
stanislav-az <st.zhdanovich@gmail.com>
2022-11-27 17:06:38 +0700

    add console bot loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    run_console_bot();
+}
+
+fn run_console_bot() {
+    loop {
+        console_bot_cycle()
+    }
+}
+
+fn console_bot_cycle() {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read user input");
+
+    println!("{input}")
 }
