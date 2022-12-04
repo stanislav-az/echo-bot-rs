9ab974d7f5c4455ddc4ecc36ad90e8ee5c7960b6
stanislav-az <st.zhdanovich@gmail.com>
2022-12-04 20:15:50 +0700

    add api methods module

--- src/telegram_bot.rs
+++ src/telegram_bot.rs
@@ -1,10 +1,10 @@
-use serde::de::DeserializeOwned;
-
 pub mod api_types;
+pub mod api_methods;
 pub mod domain_types;
 pub mod error;
 
 use api_types::*;
+use self::api_methods::{get_updates, send_sticker, send_message};
 use domain_types::*;
 pub use error::TelegramBotError;
 
@@ -13,41 +13,6 @@ pub fn one_communication_cycle(bot_token: &String) -> Result<(), TelegramBotErro
         .into_iter()
         .try_for_each(|u| handle_update(bot_token, u))?;
     Ok(())
-}
-
-pub fn get_updates(bot_token: &String) -> Result<TelegramUpdates, TelegramBotError> {
-    let resp = ureq::get(&mk_telegram_api_url(bot_token, "getUpdates"))
-        .call()
-        .map_err(TelegramBotError::HttpClient)?;
-    parse_response(resp)
-}
-
-pub fn send_message(
-    bot_token: &String,
-    chat_id: u64,
-    text: String,
-) -> Result<TelegramMessage, TelegramBotError> {
-    let resp = ureq::post(&mk_telegram_api_url(bot_token, "sendMessage"))
-        .send_json(ureq::json!({
-            "chat_id": chat_id,
-            "text": text
-        }))
-        .map_err(TelegramBotError::HttpClient)?;
-    parse_response(resp)
-}
-
-pub fn send_sticker(
-    bot_token: &String,
-    chat_id: u64,
-    file_id: String,
-) -> Result<TelegramMessage, TelegramBotError> {
-    let resp = ureq::post(&mk_telegram_api_url(bot_token, "sendSticker"))
-        .send_json(ureq::json!({
-            "chat_id": chat_id,
-            "sticker": file_id
-        }))
-        .map_err(TelegramBotError::HttpClient)?;
-    parse_response(resp)
 }
 
 pub fn handle_update(bot_token: &String, update: TelegramUpdate) -> Result<(), TelegramBotError> {
@@ -67,19 +32,4 @@ pub fn handle_update(bot_token: &String, update: TelegramUpdate) -> Result<(), T
             }
         },
     }
-}
-
-pub fn mk_telegram_api_url(bot_token: &String, method_name: &str) -> String {
-    let mut url = String::from("https://api.telegram.org/bot");
-    url.push_str(bot_token);
-    url.push('/');
-    url.push_str(method_name);
-    url
-}
-
-pub fn parse_response<T: DeserializeOwned>(resp: ureq::Response) -> Result<T, TelegramBotError> {
-    resp.into_json::<TelegramResponse<T>>()
-        .map_err(TelegramBotError::Serialization)?
-        .into_result()
-        .map_err(TelegramBotError::Api)
 }
